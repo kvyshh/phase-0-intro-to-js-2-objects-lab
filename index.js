992bce29224d68dfe1d91cbc5e4c2aa829835755
Kindred Lee <kindred4lee@gmail.com>
2021-10-05 08:03:46 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {};
+employee.name = "Kindred";
+employee.streetAddress = "38 Harriet";
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+    const newEmployee = updateEmployeeWithKeyAndValue(newEmployee, "name", "Sam");
+    newEmployee;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+const newerEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "11 Broadway");
+newerEmployee;
+
+function deleteFromEmployeeByKey(employee, key) {
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
